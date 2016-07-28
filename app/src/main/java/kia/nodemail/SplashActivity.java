bc5746507fbf85a2c343cea6554cd71fc5a3ad87
gleoozhk <iengintr@gmail.com>
2016-07-28 12:54:38 +0300

    Splash added.

--- app/src/main/java/kia/nodemail/SplashActivity.java
+++ app/src/main/java/kia/nodemail/SplashActivity.java
@@ -1,18 +1,25 @@
 package kia.nodemail;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.RelativeLayout;
 
-public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+import com.github.rahatarmanahmed.cpv.CircularProgressView;
+
+public class SplashActivity extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
